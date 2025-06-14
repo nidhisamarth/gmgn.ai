53a0866f953b4d061bfa323f8027105f9de348c8
Nidhi Samarth <144961769+nidhisamarth@users.noreply.github.com>
2025-06-14 18:31:51 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import New from "./pages/New";
+import Trending from "./pages/Trending";
+import CopyTrade from "./pages/CopyTrade";
+import Monitor from "./pages/Monitor";
+import Follow from "./pages/Follow";
+import Holding from "./pages/Holding";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,6 +23,12 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/copytrade" element={<CopyTrade />} />
+          <Route path="/monitor" element={<Monitor />} />
+          <Route path="/follow" element={<Follow />} />
+          <Route path="/holding" element={<Holding />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
